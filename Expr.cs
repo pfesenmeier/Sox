de7b2d26ef844f0afe3117fd7d19b066816dd630
Paul Fesenmeier <pfesenmeier@gmail.com>
2023-11-12 15:11:35 -0500

    add test class

--- Expr.cs
+++ Expr.cs
@@ -10,6 +10,8 @@ public record Literal(object value);
 
 public record Grouping(Expr expression);
 
+public record MyNewRecord();
+
 [GenerateOneOf]
 public partial class Expr : OneOfBase<Binary, Unary, Literal, Grouping>
 {
